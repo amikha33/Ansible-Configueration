6f41d37185522e1edff25d472a7df842eff3ed11
amikhail <46167070+amikha33@users.noreply.github.com>
2023-12-09 17:35:41 +0200

    Create update_packages.py

---
+++ update_packages.py
@@ -0,0 +1,25 @@
+import subprocess
+
+def run_ansible_command(tags=None):
+    command = [
+        "ansible-playbook", "update_packages.yml",
+        "--ask-become-pass"
+    ]
+
+    if tags:
+        command.extend(["--tags", tags])
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return process.returncode, stdout, stderr
+
+def main():
+    custom_tags = input("Enter custom tags (leave blank for 'update_packages'): ")
+    return_code, stdout, stderr = run_ansible_command(tags=custom_tags or "update_packages")
+
+    print(f"Ansible exit code: {return_code}")
+    print(f"Ansible stdout:\n{stdout.decode()}")
+    print(f"Ansible stderr:\n{stderr.decode()}")
+
+if __name__ == "__main__":
+    main()
